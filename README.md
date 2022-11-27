8de48b091d210ee19e23839dd9a17d32b8e39916
Manuel Cabezas <manuelcabezas@gmail.com>
2022-11-26 23:35:36 -0400

    feat: add event feature

---
+++ README.md
@@ -0,0 +1,10 @@
+## Desafio Modulo 03 Proyecto 01
+
+
+#### GithubPages
+
+[https://mdcabezas.github.io/desafio_mod03_01/](https://mdcabezas.github.io/desafio_mod03_01/)
+
+![Screenshot](./assets/img/screenshot.jpg)
+
+***
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Inicializa constantes formulario  
+// Inicializa constantes Formulario  
 const $color = document.querySelector("#color");
 const $quantity = document.querySelector("#quantity");
 const $price = document.querySelector("#price");
@@ -10,7 +10,7 @@ const $displayColor = document.querySelector("#displayColor");
 function handleChange() {
     // Obtiene total multiplicando precio por cantidad
     const amount = Number($price.textContent) * $quantity.value;
-    // Mostrar valores calculados 
+    // Muestra valores calculados e ingresados
     $displayAmount.innerHTML = amount;
     $displayQty.innerHTML = $quantity.value;
     $displayColor.style.background = $color.value;
